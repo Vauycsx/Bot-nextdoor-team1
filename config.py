d2f86d88eae692634c6b1394067b834807e0c05e
Max <gejmgejm989@gmail.com>
2026-04-26 13:12:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,16 @@
 import os
+import sys
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "6752278578"))
+if not BOT_TOKEN:
+    raise RuntimeError("BOT_TOKEN is not set")
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 WEBHOOK_PATH = "/webhook"
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # https://your-app.onrender.com/webhook
+
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+if not WEBHOOK_URL:
+    raise RuntimeError("WEBHOOK_URL is not set")
+
+ADMIN_ID = int(os.getenv("ADMIN_ID", "6752278578"))
